3472ef3543a6e5f5496ca5144362f3f60eb29dcf
Sesmi Athiyarath <sesmi.athiyarath@siemens-healthineers.com>
2024-02-17 17:50:58 +0530

    create controller as a class

--- data-privacy-vault/data_privacy_vault/app.py
+++ data-privacy-vault/data_privacy_vault/app.py
@@ -1,7 +1,8 @@
-from data_privacy_vault.app_controller import detokenize_request, tokenize_request
+from data_privacy_vault.app_controller import ApplicationController
 from flask import Flask, request
 
 app = Flask(__name__)
+controller = ApplicationController()
 
 @app.route('/hello')
 def hello():
@@ -10,12 +11,12 @@ def hello():
 @app.route('/tokenize', methods=['POST'])
 def tokenize():
     data = request.get_json()
-    return tokenize_request(data)
+    return controller.tokenize_request(data)
 
 @app.route('/detokenize', methods=['POST'])
 def detokenize():
     data = request.get_json()
-    return detokenize_request(data)
+    return controller.detokenize_request(data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
